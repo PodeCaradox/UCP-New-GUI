f3bd1d441f4012c53733c4d9b8d707899cc67acb
Gaaammmler <Gaaammmler@DESKTOP-SSLM1K5>
2020-03-14 05:24:42 +0100

    Initial Commit

---
+++ UnofficialCrusaderPatchGUI/Data/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static UCP.Utility;
+
+namespace UCP.Data
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            languages = Enum.GetValues(typeof(Languages)).Cast<Languages>().ToArray();
+        }
+
+        public void LoadUCPData()
+        {
+
+        }
+
+        private Languages actualLanguage = Languages.English;
+        public Languages ActualLanguage
+        {
+
+            get => actualLanguage;
+            set
+            {
+                actualLanguage = value;
+                this.RaisePropertyChanged("ActualLanguage");
+                SelectCulture(value);
+            }
+        }
+
+        private readonly Languages[] languages;
+        public Languages[] LanguagesComboBox
+        {
+            get => languages;
+        }
+
+    }
+}
---
+++ UnofficialCrusaderPatchGUI/Data/Utility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UCP
+{
+    public static class Utility
+    {
+        public enum Languages { English, Deutsch, Русский, Polski };
+        public static void SelectCulture(Languages language)
+        {
+            var dictionaryList = Application.Current.Resources.MergedDictionaries;
+
+            object dummy;
+            var resourceDictionary = dictionaryList.FirstOrDefault(d => d.Contains(language.ToString()) == true);
+            if (resourceDictionary != null)
+            {
+                Application.Current.Resources.MergedDictionaries.Remove(resourceDictionary);
+                Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+            }
+        }
+
+        public static String GetText(String key)
+        {
+            var dummy = Application.Current.Resources["key"];
+            return dummy.ToString();
+        }
+    }
+
+
+}
